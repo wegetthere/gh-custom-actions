b395424ed8de4654eb57f01512682de8a761eeb4
reivson <reivsonlopes@gmail.com>
2025-09-08 00:07:50 -0300

    feat: add S3 deployment action and integrate into workflow

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const core = require('actions/exec')
+
+function run() {
+  // 1) Get inputs
+  const bucket = core.getInput('bucket', { required: true })
+  const bucketRegion = core.getInput('bucket-region', { required: true })
+  const distFolder = core.getInput('dist-folder', { required: true })
+
+  // 2) Upload files to S3
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+
+  core.notice('Hello from my custom JavaScript action')
+  // Your code here
+}
+
+run();
